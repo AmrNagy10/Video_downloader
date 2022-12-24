8a8f42940c528910c6420bc4f9b04815337fa801
amr <AmrNagy10>
2022-12-24 18:15:55 +0200

    24 , DCE

--- save_details.py
+++ save_details.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 def save_details_fun(URL , name):
     full_name = name + "Detiles.text"
-    text_d = str(f"title is : {name} \n channal name : {YouTube(URL).author}  \n Discribtion is : {YouTube(URL).description} \n Link for channal is : {YouTube(URL).channel_url}  \n rating is : {YouTube(URL).rating} , Linke for video is : {URL}")
+    text_d = str(f"title is : {name} \nchannal name : {YouTube(URL).author}  \nDiscribtion is : {YouTube(URL).description} \nLink for channal is : {YouTube(URL).channel_url}  \nrating is : {YouTube(URL).rating} \nLinke for video is : {URL}")
     with open("Downlaoded\\" + full_name , 'wb') as f:
         f.write("{}".format(text_d).encode())
 
